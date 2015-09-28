96e74a0f94b557b51772ef41e3e98e4e9767c736
Renaud Sauvain <renaud@rsc.technology>
2015-09-28 12:51:02 +0800

    java fixes 4

--- www/hasGooglePlay.js
+++ www/hasGooglePlay.js
@@ -1,9 +1,20 @@
 var exec = require('cordova/exec');
 
-module.exports = function hasStatusBar(callback) {
-  return exec(function (has) {
-    callback(has === "true");
-  }, function (err) {
-    callback(undefined, err);
-  }, "HasGooglePlay", "hasGooglePlay", []);
+module.exports = {
+  hasGooglePlay: undefined,
+  check: function checkGooglePlay(callback) {
+    this.hasGooglePlay = undefined;
+    var that = this;
+
+    exec(function (has) {
+      that.hasGooglePlay = (has === "true");
+      if (typeof callback == 'function')
+        callback(that.hasGooglePlay);
+    }, function (err) {
+      if (typeof callback == 'function')
+        callback(undefined, err);
+    }, "HasGooglePlay", "hasGooglePlay", []);
+  }
 };
+
+module.exports.check();
