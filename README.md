794ffc7e85adaa24f6deb04e10c43e8f6bd9a630
Renaud Sauvain <renaud@rsc.technology>
2015-09-28 11:50:52 +0800

    fix

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Has-Google-Play
 ======
 
-> This plugin let your cordova javascript app to know if the Android device has google play installed.
+    > This plugin let your cordova javascript app to know if the Android device has google play installed.
 
 
 ## Installation
@@ -12,9 +12,8 @@ cordova plugin add https://github.com/sauvainr/cordova-has-google-play
 
 ## Usage
 
+    if(!window.hasGooglePlay()){
 
-  if(!window.hasGooglePlay()){
+      .. auto-update logic ..
 
-    .. auto-update logic ..
-
-  }
+    }
--- www/hasGooglePlay.js
+++ www/hasGooglePlay.js
@@ -1,18 +1,7 @@
 var exec = require('cordova/exec');
 
-// prime it
-exec(function (res) {
-	if (typeof res == 'object') {
-		if (res.type == 'tap') {
-			cordova.fireWindowEvent('statusTap');
-		}
-	} else {
-		StatusBar.isVisible = res;
-	}
-}, null, "StatusBar", "_ready", []);
-
 module.exports = function hasStatusBar(callback) {
-	return exec(callback, function (err) {
-		callback(err);
-	}, "HasGooglePlay", "hasGooglePlay", []);
+  return exec(callback, function (err) {
+    callback(undefined, err);
+  }, "HasGooglePlay", "hasGooglePlay", []);
 };
